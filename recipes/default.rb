a5d47e681d18cd19632ebf920f2804499ae9a544
wb <wpjtek@yahoo.com>
2015-01-22 22:44:56 +0100

    Both tests are green

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+config_file = node['myapp_example']['config_file'] 
+config_file_content = node['myapp_example']['config_content']
+
 package "vim-nox" do
 	action :install
+end
+
+file config_file do
+  	action :create
+  	content config_file_content
 end
